55eea2ea74687d5554d03e86fffb109d6bc4f0c4
Jinsol Kim <zzolain@gmail.com>
2022-02-13 09:00:38 +0900

    Initial Commit

---
+++ Shared/algorithmApp.swift
@@ -0,0 +1,17 @@
+//
+//  algorithmApp.swift
+//  Shared
+//
+//  Created by 김진솔 on 2022/02/13.
+//
+
+import SwiftUI
+
+@main
+struct algorithmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
